d195d8b00e8adbf14aea6ae8410564df64896deb
nelvin-hmar <109198308+nelvin-tf@users.noreply.github.com>
2025-04-03 23:37:07 +0530

    env configurable added to bypass https and replace with localhost

--- classes/utils.py
+++ classes/utils.py
@@ -1,3 +1,14 @@
+import os
+import re
+
 def clean_json_string(json_string):
     return json_string.replace("```json", "").replace("```", "")
+
+def replace_server_url_with_localhost(url):
+    if os.getenv("ALWAYS_USE_LOCALHOST"):
+        """Replaces any HTTP URL with http://localhost:3000/."""
+        url = url.replace("https://", "http://")
+        return re.sub(r"http://[^/]+/", "http://localhost:3000/", url)
+
+    return url
 
